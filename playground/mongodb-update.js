cdf6f45a8f12aaaa208ff81499a495c04d4e4ca1
Keegan Conway <conwaykc@knights.ucf.edu>
2018-10-16 16:29:12 -0400

    mongo update support

---
+++ playground/mongodb-update.js
@@ -0,0 +1,40 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp')
+
+  // findOneAndUpdate
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5bc62b9295a7daee0c44c27b')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((res) => {
+  //   console.log(res);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5bb3bab945e66d34e4b26c7b')
+  }, {
+    $set: {
+      name: 'Keegan'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((res) => {
+    console.log(res);
+  });
+
+  // client.close();
+});
