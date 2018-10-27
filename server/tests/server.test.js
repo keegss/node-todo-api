979073c4247205883b15bf2524ccc78a3e5e4126
Keegan Conway <conwaykc@knights.ucf.edu>
2018-10-27 12:49:23 -0400

    test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -93,11 +93,48 @@ describe('GET /todos/:id', () => {
   });
 
   it('should return 404 for non-object ids', (done) => {
-    // /todos/123
     request(app)
       .get('/todos/123')
       .expect(404)
       .end(done);
   })
 
+});
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hecId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hecId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hecId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hecId).then((todo) => {
+          expect(todo).toBeFalsy();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var testId = new ObjectID();
+    request(app)
+      .delete(`/todos/${testId.toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if object id is invalid', (done) => {
+    request(app)
+      .delete('/todos/123')
+      .expect(404)
+      .end(done);
+  });
 });
